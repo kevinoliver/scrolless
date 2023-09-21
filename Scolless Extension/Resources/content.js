ab7febfafe712927fb7ec063c64dc457c0342ed1
Kevin Oliver <kevinoliver@gmail.com>
2023-09-21 11:24:28 -0700

    v1 which kinda works

--- Scolless Extension/Resources/content.js
+++ Scolless Extension/Resources/content.js
@@ -1,7 +1,23 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
+//browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
+//    console.log("Received response: ", response);
+//});
+//
+//browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//    console.log("Received request: ", request);
+//});
+console.log("Hello from my Scrolless extension!");
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+// This script listens for keyboard events and modifies the browser's scrolling behavior.
+window.addEventListener("keydown", function(event) {
+    // ko todo: make sure not shift+space
+    if (event.key === "PageDown" || event.key === " ") {
+        console.log("scrolling less for '" + event.key + "'");
+        // Scroll the page 100 pixels back up
+        window.scrollBy(0, -100);
+    } else if (event.key === "PageUp") {
+        // ko todo: shift+space modifier
+        console.log("scrolling less for '" + event.key + "'");
+        // Scroll the page 100 pixels back down
+        window.scrollBy(0, +100);
+    }
 });
