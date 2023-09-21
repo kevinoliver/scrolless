7e81843d04ceb3bc16990a5d19f18b1bb72577c8
Kevin Oliver <kevinoliver@gmail.com>
2023-09-21 12:06:12 -0700

    only for specific sites

--- Scolless Extension/Resources/content.js
+++ Scolless Extension/Resources/content.js
@@ -1,20 +1,27 @@
-// console.log("Hello from my Scrolless extension!");
-
 // possible solution which srolls only once:
 // https://stackoverflow.com/questions/69257481/can-i-change-the-amount-that-pagedown-pageup-scrolls-the-page-to-avoid-some-con
 
-// ko todo: only enable for some sites and control the scroll amount by site
+// ko todo: make this a hashmap
+var delta = 0;
+var hostname = window.location.hostname;
+if (hostname === "www.sfchronicle.com") {
+   delta = 90;
+} else if (hostname === "theathletic.com") {
+   delta = 30;
+}
+console.log("Scrolless delta set to " + delta + " for hostname " + hostname);
 
-// listens for keyboard events and modifies the browser's scrolling behavior.
-window.addEventListener("keydown", function(event) {
-    var delta = 90;
-    if (event.key === "PageDown" || (event.key === " " && !event.getModifierState("Shift"))) {
-        console.log("scrolling down less for '" + event.key + "'");
-        // Scroll the page back up
-        window.scrollBy(0, -delta);
-    } else if (event.key === "PageUp" || (event.key === " " && event.getModifierState("Shift"))) {
-        console.log("scrolling up less for '" + event.key + "'");
-        // Scroll the page back down
-        window.scrollBy(0, delta);
-    }
-});
+if (delta != 0) {
+    // listens for keyboard events and modifies the browser's scrolling behavior.
+    window.addEventListener("keydown", function(event) {
+        if (event.key === "PageDown" || (event.key === " " && !event.getModifierState("Shift"))) {
+            console.log("scrolling down less for '" + event.key + "'");
+            // Scroll the page back up
+            window.scrollBy(0, -delta);
+        } else if (event.key === "PageUp" || (event.key === " " && event.getModifierState("Shift"))) {
+            console.log("scrolling up less for '" + event.key + "'");
+            // Scroll the page back down
+            window.scrollBy(0, delta);
+        }
+    });
+}
